d31c47a4b6df53e10a862c80810833665d755f2a
Leonardo David Sánchez Luna <ldsanchez@MBP-de-Leonardo.fibertel.com.ar>
2020-05-01 21:20:18 -0300

    Cambio de server

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ mlflow.set_tracking_uri(remote_server_uri)
 #client = mlflow.tracking.MlflowClient()
 #client.create_experiment('your_experiment_name')
 #mlflow.start_run(run_id=None, experiment_id=3, run_name="Test", nested=False)
-mlflow.set_experiment('SI_Sent_ES_Political')
-#mlflow.source.type="CONSOLE"
+#mlflow.set_experiment('SI_Sent_ES_Political')
+mlflow.source.type="CONSOLE"
 mlflow.start_run(experiment_id=1)
 
 tags = {
